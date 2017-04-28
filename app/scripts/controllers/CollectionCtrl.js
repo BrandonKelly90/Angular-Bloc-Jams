881b753358b463813e8e471033b30a049623853f
Brandon Kelly <brandonmichaelkelly@gmail.com>
2017-04-27 17:24:33 -0700

    added Collection & Landing page controllers in checkpoint 5

---
+++ app/scripts/controllers/CollectionCtrl.js
@@ -0,0 +1,11 @@
+(function(){
+    funtion CollectionCtrl() {
+        this.albums = [];
+        for (var i = 0; i < 12; i++){
+            this.albums.push(angular.copy(albumPicasso));
+        }
+    }
+    angular
+        .module('blocJams')
+        .controller('CollectionCtrl', CollectionCtrl);
+})();
