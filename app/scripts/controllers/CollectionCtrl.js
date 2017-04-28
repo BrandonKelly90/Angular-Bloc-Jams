eefe0f5fa1743f3aca2fb2b36c2b238241c0abfc
Brandon Kelly <brandonmichaelkelly@gmail.com>
2017-04-27 17:30:55 -0700

    corrected typographical issues that prevented the code from running

--- app/scripts/controllers/CollectionCtrl.js
+++ app/scripts/controllers/CollectionCtrl.js
@@ -1,5 +1,5 @@
 (function(){
-    funtion CollectionCtrl() {
+    function CollectionCtrl() {
         this.albums = [];
         for (var i = 0; i < 12; i++){
             this.albums.push(angular.copy(albumPicasso));
