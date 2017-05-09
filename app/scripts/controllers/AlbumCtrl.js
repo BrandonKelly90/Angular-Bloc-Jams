711a54e525403617ef866ec8080cae5b32da8056
Brandon Kelly <brandonmichaelkelly@gmail.com>
2017-05-08 20:35:07 -0700

    added release info property to concat album year & label

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,8 +1,9 @@
 (function(){
     function AlbumCtrl() {
         this.albumData = angular.copy(albumPicasso);
+        this.albumData.releaseInfo = this.albumData.year + ' ' + this.albumData.label;
         }
-        
+
     angular
         .module('blocJams')
         .controller('AlbumCtrl', AlbumCtrl);
